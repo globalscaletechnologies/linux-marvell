6701e8bc96478a05f4e092261bf67c52774639d5
Jason Hung <jhung@globalscaletechnologies.com>
2019-04-23 13:52:02 +0800

    leds: ledtrig-timer: fix led_delay paramter reset issue
    using local variable instead of blink_delay_on/off variable to prevent
    blink_delay_on/off be overwritten by led_stop_software_blink().

--- drivers/leds/trigger/ledtrig-timer.c
+++ drivers/leds/trigger/ledtrig-timer.c
@@ -30,14 +30,15 @@ static ssize_t led_delay_on_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t size)
 {
 	struct led_classdev *led_cdev = dev_get_drvdata(dev);
-	unsigned long state;
+	unsigned long state, delay_off;
 	ssize_t ret = -EINVAL;
 
 	ret = kstrtoul(buf, 10, &state);
 	if (ret)
 		return ret;
 
-	led_blink_set(led_cdev, &state, &led_cdev->blink_delay_off);
+	delay_off = led_cdev->blink_delay_off;
+	led_blink_set(led_cdev, &state, &delay_off);
 	led_cdev->blink_delay_on = state;
 
 	return size;
@@ -55,14 +56,15 @@ static ssize_t led_delay_off_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t size)
 {
 	struct led_classdev *led_cdev = dev_get_drvdata(dev);
-	unsigned long state;
+	unsigned long state, delay_on;
 	ssize_t ret = -EINVAL;
 
 	ret = kstrtoul(buf, 10, &state);
 	if (ret)
 		return ret;
 
-	led_blink_set(led_cdev, &led_cdev->blink_delay_on, &state);
+	delay_on = led_cdev->blink_delay_on;
+	led_blink_set(led_cdev, &delay_on, &state);
 	led_cdev->blink_delay_off = state;
 
 	return size;
